ea809fe61e2ac2c8c3cbf0ecbd9abe57d23a2171
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-02 18:35:00 +0100

    plugin: `tailwind-inter`

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   darkMode: "class",
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    interFontFeatures: {
+      default: ['calt', 'liga', 'kern'],
+      numeric: ['tnum', 'salt', 'ss02'],
+      text: ['ss01', 'calt', 'liga', 'kern']
+    },
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('tailwindcss-font-inter')({ // it's plugin's default settings
+      a: -0.0223,
+      b: 0.185,
+      c: -0.1745,
+      baseFontSize: 160,
+      baseLineHeight: 1.5,
+      importFontFace: true,
+      disableUnusedFeatures: false
+    }),
+    require('@tailwindcss/typography')
+  ]
 };
