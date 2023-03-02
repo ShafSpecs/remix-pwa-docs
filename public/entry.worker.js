65293e953882a1807fc9bdc2479eb6803e0df187
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-02 12:47:31 +0100

    Extra styling

--- public/entry.worker.js
+++ public/entry.worker.js
@@ -211,29 +211,3 @@ self.addEventListener("fetch", (event) => {
 async function appHandleFetch(event, { error, response }) {
   return response;
 }
-/*! Bundled license information:
-
-@remix-run/server-runtime/dist/esm/responses.js:
-  (**
-   * @remix-run/server-runtime v1.12.0
-   *
-   * Copyright (c) Remix Software Inc.
-   *
-   * This source code is licensed under the MIT license found in the
-   * LICENSE.md file in the root directory of this source tree.
-   *
-   * @license MIT
-   *)
-
-@remix-run/server-runtime/dist/esm/index.js:
-  (**
-   * @remix-run/server-runtime v1.12.0
-   *
-   * Copyright (c) Remix Software Inc.
-   *
-   * This source code is licensed under the MIT license found in the
-   * LICENSE.md file in the root directory of this source tree.
-   *
-   * @license MIT
-   *)
-*/
