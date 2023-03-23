8258fa31b1baa02c1459507e9ba4b27a6a4923b2
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-23 20:01:01 +0100

    chore: dynamic import/export of `esm` modules

---
+++ app/exports/esm-modules.js
@@ -0,0 +1,9 @@
+module.exports = {
+  importGfm: async () => import('remark-gfm'),
+  importEmoji: async () => import('remark-emoji'),
+  importSlug: async () => import('rehype-slug'),
+  importPrism: async () => import('rehype-prism-plus/common'),
+  importAutolink: async () => import('rehype-autolink-headings'),
+  importOembed: async () => import('@agentofuser/remark-oembed'),
+  importHint: async () => import('remark-hint'),
+}
