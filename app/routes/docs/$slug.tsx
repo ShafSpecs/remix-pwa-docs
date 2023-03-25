cdbc0f967f789b01b9800a160a14932e97b6bfe0
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-25 14:13:07 +0100

    docs: Working on page layout

--- app/routes/docs/$slug.tsx
+++ app/routes/docs/$slug.tsx
@@ -8,9 +8,17 @@ import type { LoaderFunction } from "@remix-run/node"
 
 export const loader: LoaderFunction = async () => {
   const s = `
+  ---
+title: Example Post
+published: 2021-02-13
+description: This is some description
+---
+
   # GFM
   
   ## Autolink literals
+
+  https://youtu.be/mpnh1YTT66w
   
   www.example.com, https://example.com, and contact@example.com.
   
@@ -54,19 +62,22 @@ export const loader: LoaderFunction = async () => {
   return code;
 }
 
-function Post({ code, frontmatter }: any) {
+function Doc({ code, frontmatter }: any) {
   // it's generally a good idea to memoize this function call to
   // avoid re-creating the component every render.
   const Component = useMemo(() => getMDXComponent(code), [code])
   return (
-    <>
-      <header>
-        <h1></h1>
-      </header>
-      <main>
-        <Component />
-      </main>
-    </>
+    <div className='min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16'>
+      <article>
+        <header className="mb-9 space-y-1">
+          <p className="font-display text-sm font-medium text-sky-500">Introduction</p>
+          <h1 className="font-display text-3xl tracking-tight text-slate-900 dark:text-white">{frontmatter.title}</h1>
+        </header>
+        <main className='prose prose-slate max-w-none dark:prose-invert dark:text-slate-400 prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem] prose-lead:text-slate-500 dark:prose-lead:text-slate-400 prose-a:font-semibold dark:prose-a:text-sky-400 prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800'>
+          <Component />
+        </main>
+      </article>
+    </div>
   )
 }
 
@@ -75,7 +86,7 @@ export default function DocPage() {
 
   return (
     <ClientOnly children={() =>
-      <Post
+      <Doc
         code={loaderData.code}
         frontmatter={loaderData.frontmatter}
       />
