15330f98c72062ca42970502e088b0d8fe4808e0
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-25 16:16:06 +0100

    chore: Updated sidebar

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLocation, useMatches } from "@remix-run/react";
+import { Links, LiveReload, Meta, NavLink, Outlet, Scripts, ScrollRestoration, useLocation, useMatches } from "@remix-run/react";
 import { useLocalStorage } from "usehooks-ts";
 import Hero from "./components/hero";
 import Header from "./components/header";
@@ -27,6 +27,98 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1"
 });
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const list: {
+  topic: string;
+  children: {
+    slug: string;
+    name: string;
+  }[]
+}[] = [
+    {
+      topic: "Introduction",
+      children: [
+        {
+          slug: "/",
+          name: "Getting Started"
+        },
+        {
+          slug: "/docs/installation",
+          name: "Installation"
+        },
+        {
+          slug: "/docs/gotchas",
+          name: "Gotchas"
+        }
+      ]
+    },
+    {
+      topic: "Concepts",
+      children: [
+        {
+          slug: "/docs/routes",
+          name: "Routes"
+        },
+        {
+          slug: "/docs/data",
+          name: "Data"
+        },
+        {
+          slug: "/docs/environment-variables",
+          name: "Environment Variables"
+        },
+        {
+          slug: "/docs/links",
+          name: "Links"
+        },
+        {
+          slug: "/docs/meta",
+          name: "Meta"
+        },
+        {
+          slug: "/docs/scripts",
+          name: "Scripts"
+        },
+        {
+          slug: "/docs/styles",
+          name: "Styles"
+        }
+      ]
+    },
+    {
+      topic: "API",
+      children: [
+        {
+          slug: "/docs/remix",
+          name: "Remix"
+        },
+        {
+          slug: "/docs/remix-run",
+          name: "Remix Run"
+        },
+        {
+          slug: "/docs/remix-react",
+          name: "Remix React"
+        },
+        {
+          slug: "/docs/remix-react-router",
+          name: "Remix React Router"
+        },
+        {
+          slug: "/docs/remix-server",
+          name: "Remix Server"
+        },
+        {
+          slug: "/docs/remix-utils",
+          name: "Remix Utils"
+        }
+      ]
+    }
+  ]
 
 export default function App() {
   let location = useLocation();
@@ -132,6 +224,7 @@ export default function App() {
           setSelectedTheme={setSelectedTheme}
           closed={closed}
           setClosed={setClosed}
+          list={list}
         />
         {location.pathname == "/" && <Hero />}
         <div className="relative flex justify-center mx-auto max-w-[88rem] sm:px-2 lg:px-8 xl:px-12">
@@ -142,58 +235,35 @@ export default function App() {
             <div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto overflow-x-hidden py-16 pl-0.5">
               <nav className="w-64 pr-8 text-base lg:text-sm xl:w-72 xl:pr-16">
                 <ul className="space-y-9">
-                  <li>
-                    <h2 className="font-medium font-display text-slate-900 dark:text-white">Introduction</h2>
-                    <ul className="mt-2 space-y-2 border-l-2 border-slate-100 dark:border-slate-800 lg:mt-4 lg:space-y-4 lg:border-slate-200">
-                      <li className="relative">
-                        <a
-                          className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full font-semibold text-sky-500 before:bg-sky-500"
-                          href="/"
-                        >
-                          Getting started
-                        </a>
+                  {list.map((el) => {
+                    return (
+                      <li key={el.topic}>
+                        <h2 className="font-medium font-display text-slate-900 dark:text-white">{el.topic}</h2>
+                        <ul className="mt-2 space-y-2 border-l-2 border-slate-100 dark:border-slate-800 lg:mt-4 lg:space-y-4 lg:border-slate-200">
+                          {el.children.map((sub) => {
+                            return (
+                              <li className="relative" key={sub.slug}>
+                                <NavLink to={sub.slug} end={true} >
+                                  {({ isActive }) => (
+                                    <span className={classNames(
+                                      "block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full",
+                                      isActive ?
+                                        "font-semibold text-sky-500 before:bg-sky-500" :
+                                        "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300")}>
+                                      {sub.name}
+                                    </span>
+                                  )}
+                                </NavLink>
+                              </li>
+                            )
+                          })}
+                        </ul>
                       </li>
-                      <li className="relative">
-                        <a
-                          className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
-                          href="/docs/installation"
-                        >
-                          Installation
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <h2 className="font-medium font-display text-slate-900 dark:text-white">Core concepts</h2>
-                    <ul className="mt-2 space-y-2 border-l-2 border-slate-100 dark:border-slate-800 lg:mt-4 lg:space-y-4 lg:border-slate-200">
-                      <li className="relative">
-                        <a
-                          className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
-                          href="/docs/understanding-caching"
-                        >
-                          Understanding caching
-                        </a>
-                      </li>
-                      <li className="relative">
-                        <a
-                          className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
-                          href="/docs/predicting-user-behavior"
-                        >
-                          Predicting user behavior
-                        </a>
-                      </li>
-                      <li className="relative">
-                        <a
-                          className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
-                          href="/docs/basics-of-time-travel"
-                        >
-                          Basics of time-travel
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
+                    )
+                  })}
                 </ul>
               </nav>
+
             </div>
           </div>
           <Outlet />
