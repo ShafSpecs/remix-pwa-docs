c7eee30ee06dc5daef89703066daaf4794b8430a
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-25 16:16:56 +0100

    chore: Updated docs page

--- app/routes/docs/$slug.tsx
+++ app/routes/docs/$slug.tsx
@@ -1,10 +1,10 @@
-import { useMemo, useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { mdxToHtml } from "~/mdx/mdx.server";
 import { getMDXComponent } from "mdx-bundler/client";
 import { ClientOnly } from 'remix-utils';
 import { useLoaderData } from '@remix-run/react';
 
-import type { LoaderFunction } from "@remix-run/node"
+import type { LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = async () => {
   const s = `
@@ -12,13 +12,24 @@ export const loader: LoaderFunction = async () => {
 title: Example Post
 published: 2021-02-13
 description: This is some description
+prompt: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, sint officia distinctio sapiente magnam porro. Asperiores voluptatum ea adipisci eveniet?
 ---
+
+import Warn from './warn.tsx'
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente accusantium magni aspernatur illo dicta. Mollitia commodi sint cumque dolorum eveniet obcaecati fuga atque porro, 
+tempora libero, fugit excepturi hic nulla quo unde voluptatum aspernatur animi voluptates minus numquam magni quisquam. Maiores optio iusto asperiores accusamus!
 
   # GFM
   
   ## Autolink literals
 
-  https://youtu.be/mpnh1YTT66w
+  :+1:
+
+  ### Warning
+
+  <Warn>Hello from MDX!</Warn>
+  🎈
   
   www.example.com, https://example.com, and contact@example.com.
   
@@ -42,12 +53,6 @@ description: This is some description
   * [ ] to do
   * [x] done
   
-  !> Here is a tip.
-  
-  ?> And a warning.
-  
-  x> Or an error.
-  
   \`\`\`js {1,3-4} showLineNumbers
   function fancyAlert(arg) {
     if (arg) {
@@ -63,9 +68,8 @@ description: This is some description
 }
 
 function Doc({ code, frontmatter }: any) {
-  // it's generally a good idea to memoize this function call to
-  // avoid re-creating the component every render.
-  const Component = useMemo(() => getMDXComponent(code), [code])
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+
   return (
     <div className='min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16'>
       <article>
@@ -73,8 +77,48 @@ function Doc({ code, frontmatter }: any) {
           <p className="font-display text-sm font-medium text-sky-500">Introduction</p>
           <h1 className="font-display text-3xl tracking-tight text-slate-900 dark:text-white">{frontmatter.title}</h1>
         </header>
-        <main className='prose prose-slate max-w-none dark:prose-invert dark:text-slate-400 prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem] prose-lead:text-slate-500 dark:prose-lead:text-slate-400 prose-a:font-semibold dark:prose-a:text-sky-400 prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800'>
+        <main className='prose table-auto prose-slate max-w-none dark:prose-invert dark:text-slate-400 prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem] prose-lead:text-slate-500 dark:prose-lead:text-slate-400 prose-a:font-semibold dark:prose-a:text-sky-400 prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800'>
+          <p>
+            {frontmatter.prompt}
+          </p>
+          <hr />
           <Component />
+        </main>
+      </article>
+      <dl className="mt-12 flex border-t border-slate-200 pt-6 dark:border-slate-800">
+        <div className="ml-auto text-right">
+          <dt className="font-display text-sm font-medium text-slate-900 dark:text-white">
+            Next
+          </dt>
+          <dd className="mt-1">
+            <a
+              className="text-base font-semibold text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+              href="/docs/installation"
+            >
+              Installation{/* */} <span aria-hidden="true">→</span>
+            </a>
+          </dd>
+        </div>
+      </dl>
+
+    </div>
+  )
+}
+
+const Skeleton = () => {
+  return(
+    <div className='min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16'>
+      <article>
+        <header className="mb-9 space-y-1">
+          <p className="font-display text-sm font-medium text-sky-500">Introduction</p>
+          <h1 className="font-display text-3xl tracking-tight text-slate-900 dark:text-white">{}</h1>
+        </header>
+        <main className='prose table-auto prose-slate max-w-none dark:prose-invert dark:text-slate-400 prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem] prose-lead:text-slate-500 dark:prose-lead:text-slate-400 prose-a:font-semibold dark:prose-a:text-sky-400 prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800'>
+          <p>
+            {/* {frontmatter.prompt} */}
+          </p>
+          <hr />
+          {/* <Component /> */}
         </main>
       </article>
     </div>
@@ -85,11 +129,13 @@ export default function DocPage() {
   const loaderData = useLoaderData();
 
   return (
-    <ClientOnly children={() =>
-      <Doc
-        code={loaderData.code}
-        frontmatter={loaderData.frontmatter}
-      />
-    } />
+    <ClientOnly
+      fallback={<Skeleton />}
+      children={() =>
+        <Doc
+          code={loaderData.code}
+          frontmatter={loaderData.frontmatter}
+        />
+      } />
   );
 }
