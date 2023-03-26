3e0ecb49d525bbd731592fa0adf00a828c44b7af
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-26 16:15:39 +0100

    meta: Updated meta info

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix PWA Docs",
   viewport: "width=device-width,initial-scale=1"
 });
 
