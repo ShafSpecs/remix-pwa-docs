f264cc8abed9453a197bca56ff3b09beca9d2da8
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-06-08 15:09:42 +0100

    fix: mdx bump

--- app/exports/esm-modules.js
+++ app/exports/esm-modules.js
@@ -3,5 +3,6 @@ module.exports = {
   importEmoji: async () => import('remark-emoji'),
   importSlug: async () => import('rehype-slug'),
   importPrism: async () => import('rehype-prism-plus/common'),
+  importPrismOG: async () => import('rehype-prism'),
   importAutolink: async () => import('rehype-autolink-headings')
 }
