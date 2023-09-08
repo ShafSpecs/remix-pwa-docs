38f88eb4d7b5354e1df7d55d6ea515034ee7ad2a
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-08 22:33:50 +0100

    chore: exported local `remark` plugins

--- app/exports/esm-modules.js
+++ app/exports/esm-modules.js
@@ -1,8 +1,10 @@
 module.exports = {
   importGfm: async () => import('remark-gfm'),
+  importToC: async () => import('../remark/toc'),
+  importExample: async () => import('../remark/code'),
+  importHighlighter: async () => import('../remark/highlight'),
   importEmoji: async () => import('remark-emoji'),
   importSlug: async () => import('rehype-slug'),
   importPrism: async () => import('rehype-prism-plus/common'),
   importPrismOG: async () => import('rehype-prism'),
-  importAutolink: async () => import('rehype-autolink-headings')
 }
