62d36ca1a8e7ba7882b59d137ef32d6e728968cd
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-08 17:45:58 +0100

    chore: uprooted utils for old doc format

--- app/utils/ParamHelpers.tsx
+++
@@ -1,8 +0,0 @@
-import type { Params } from "@remix-run/react";
-import invariant from "tiny-invariant";
-
-export const RequireParam = (params: Params, param: string, message?: string) => {
-  const value = params[param];
-  invariant(value, message || `Missing required param: ${param}`);
-  return value;
-};
