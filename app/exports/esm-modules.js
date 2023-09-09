480bb7dd857aa1c440d60254eee22e8c363dbcc6
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-09 13:04:35 +0100

    feat: new local `remark` & `rehype` plugins

--- app/exports/esm-modules.js
+++ app/exports/esm-modules.js
@@ -1,10 +1,12 @@
 module.exports = {
   importGfm: async () => import('remark-gfm'),
+  importEmoji: async () => import('remark-emoji'),
   importToC: async () => import('../remark/toc'),
   importExample: async () => import('../remark/code'),
   importHighlighter: async () => import('../remark/highlight'),
-  importEmoji: async () => import('remark-emoji'),
   importSlug: async () => import('rehype-slug'),
   importPrism: async () => import('rehype-prism-plus/common'),
   importPrismOG: async () => import('rehype-prism'),
+  importRole: async () => import('../rehype/role'),
+  importCheckbox: async () => import('../rehype/checkbox'),
 }
