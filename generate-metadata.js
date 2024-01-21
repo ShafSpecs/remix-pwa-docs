fee567737c68b4b88f0728236fdf9535b278e83b
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-21 15:51:19 +0100

    metadata generator updated

--- generate-metadata.js
+++ generate-metadata.js
@@ -21,37 +21,24 @@ const getPostsMeta = async () => {
 
     return dirData;
   }
-
-
-  // const posts = await octokit("GET /repos/{owner}/{repo}/contents/{path}", {
-  //   owner: "ShafSpecs",
-  //   repo: "remix-pwa-docs",
-  //   path: "posts",
-  //   ref: "docs"
-  // });
-
-  // return Promise.all(posts.data.map(async (post) => {
-  //   const { download_url } = post;
-
-  //   // Skip metadata.json file
-  //   if (download_url.includes("metadata.json")) return;
-
-  //   const content = await fetch(download_url).then(res => res.text())
-  //   const { data } = grayMatter(content);
-
-  //   return data;
-  // }))
 }
 
-const getMetaDataSHA = async () => {
-  const meta = await octokit("GET /repos/{owner}/{repo}/contents/{path}", {
-    owner: "ShafSpecs",
-    repo: "remix-pwa-docs",
-    path: "posts/metadata.json",
-    ref: "control"
-  });
+const getDevPostsMeta = async () => {
+  if (process.env.NODE_ENV === "development") {
+    const pwaPath = path.join(__dirname, "posts_dev");
+    let dirData = [];
 
-  return meta.data.sha;
+    fs.readdirSync(pwaPath).forEach(file => {
+      if (file === "metadata.json") return;
+
+      const content = fs.readFileSync(path.join(pwaPath, file), "utf8");
+      const { data } = grayMatter(content);
+      dirData.push(data);
+      return data;
+    })
+
+    return dirData;
+  }
 }
 
 const metaData = async () => {
@@ -65,9 +52,6 @@ const metaData = async () => {
   ];
 
   const postsMetadata = await getPostsMeta();
-
-  // if (process.env.NODE_ENV !== "development")
-  //   sha = await getMetaDataSHA();
 
   postsMetadata.forEach((m) => {
     const section = metadata.find(e => e.name === m.section);
@@ -87,21 +71,41 @@ const metaData = async () => {
     }
   })
 
-  const content = Buffer.from(JSON.stringify(metadata, null, 2)).toString("base64");
-
-  if (process.env.NODE_ENV !== "development")
-  // await octokit('PUT /repos/{owner}/{repo}/contents/{path}', {
-  //   owner: "ShafSpecs",
-  //   repo: "remix-pwa-docs",
-  //   path: "posts/metadata.json",
-  //   branch: "control",
-  //   sha,
-  //   message: 'chore: Updated metadata.json',
-  //   content: content,
-  // })
-    console.log("Data")
-  else
     fs.writeFileSync(path.join(__dirname, "posts", "metadata.json"), JSON.stringify(metadata, null, 2).toString(), "utf-8");
 }
 
+const devMetaData = async () => {
+  const metadata = [
+    { "name": "Getting Started", position: 1, children: [] },
+    // { "name": "Routes & Worker API", position: 2, children: [] },
+    // { "name": "Hooks & Components", position: 3, children: [] },
+    // { "name": "Utilities", position: 4, children: [] },
+    // { "name": "Other API", position: 5, children: [] },
+    // { "name": "Guides", position: 6, children: [] }
+  ];
+
+  const postsMetadata = await getDevPostsMeta();
+
+  postsMetadata.forEach((m) => {
+    const section = metadata.find(e => e.name === m.section);
+
+    if (section) {
+      section.children.push({
+        title: m.title,
+        description: m.description,
+        section: m.section,
+        shortTitle: m.shortTitle,
+        position: m.position
+      })
+
+      section.children.sort((a, b) => a.position - b.position);
+
+      metadata.sort((a, b) => a.position - b.position);
+    }
+  })
+
+    fs.writeFileSync(path.join(__dirname, "posts_dev", "metadata.json"), JSON.stringify(metadata, null, 2).toString(), "utf-8");
+}
+
 metaData();
+devMetaData();
