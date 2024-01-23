f4f7853f473ffa6191531a3f0d2f2d094b9ed2ad
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-23 14:31:49 +0100

    refactor

--- app/components/layout/Sidebar.tsx
+++ app/components/layout/Sidebar.tsx
@@ -5,7 +5,6 @@ import type { Dispatch, SetStateAction } from "react";
 import { createContext, useRef, useState } from "react";
 import { useIsomorphicLayoutEffect, useMediaQuery } from "usehooks-ts";
 import { useLocation, useRouteLoaderData } from "@remix-run/react";
-import { Dialog } from "@headlessui/react";
 import { useActionKey } from "~/hooks/useActionKey";
 
 /**
@@ -162,33 +161,6 @@ export default function SidebarLayout({
           <div className="lg:pl-[19.5rem]">{children}</div>
         </div>
       </Wrapper>
-      <Dialog
-        as="div"
-        open={navIsOpen}
-        onClose={() => setNavIsOpen(false)}
-        className="fixed inset-0 z-50 overflow-y-auto lg:hidden"
-      >
-        <Dialog.Overlay className="fixed inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
-        <div className="relative bg-white w-80 max-w-[calc(100%-3rem)] p-6 dark:bg-slate-800">
-          <button
-            type="button"
-            onClick={() => setNavIsOpen(false)}
-            className="absolute z-10 flex items-center justify-center w-8 h-8 top-5 right-5 text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
-          >
-            <span className="sr-only">Close navigation</span>
-            <svg viewBox="0 0 10 10" className="w-2.5 h-2.5 overflow-visible">
-              <path
-                d="M0 0L10 10M10 0L0 10"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-          </button>
-          <Nav fallbackHref={''} />
-        </div>
-      </Dialog>
     </SidebarContext.Provider>
   )
 }
