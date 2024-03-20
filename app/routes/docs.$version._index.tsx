f61d6ad8dfe2d84389050e30cdfebc98213454b6
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-20 21:18:04 +0100

    yeeted out old routes - srsly quite clumsy

--- app/routes/docs.$version._index.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
-
-export const loader = async ({ params }: LoaderFunctionArgs) => {
-  return redirect(`/docs/main/installation`);
-};
--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,17 +1,14 @@
-import {type DataFunctionArgs} from '@remix-run/node'
-import { getPostMetaData } from '~/utils/server/aws.server'
+import type { LoaderFunctionArgs } from '@remix-run/node'
 
-export async function loader({request}: DataFunctionArgs) {
+export async function loader({ request }: LoaderFunctionArgs) {
   try {
-    await Promise.all([
-      getPostMetaData()
-    ])
+    await new Promise(resolve => setTimeout(resolve, 1000))
 
     console.log(request.url, 'healthcheck ✅')
-    
+
     return new Response('OK')
   } catch (error: unknown) {
-    console.error(request.url, 'healthcheck ❌', {error})
-    return new Response('ERROR', {status: 500})
+    console.error(request.url, 'healthcheck ❌', { error })
+    return new Response('ERROR', { status: 500 })
   }
 }
