bdda171f83748bd1412b25c85689ba474676184a
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-20 21:13:07 +0100

    chore: new + better eslint rules

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"]
-};
