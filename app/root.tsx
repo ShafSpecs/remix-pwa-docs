ffe8740e2ded345330280294448e3d4ba68b76b4
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-04-27 17:14:57 +0100

    chore: added algolia & staging this nonsense (fn)

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,15 @@ import {
   ScrollRestoration,
 } from '@remix-run/react'
 import { json } from '@remix-run/node'
+import { KBarProvider } from 'kbar'
 import type { LoaderFunctionArgs } from '@remix-run/node'
 
 import { ClientHints, getHints } from './components/ClientHint'
 import { useTheme } from './hooks/useTheme'
 import { getTheme } from './utils/server/theme.server'
 import { getVersions } from './utils/server/doc.server'
+import { Progress } from './components/Progress'
+import { Search } from './components/Search'
 
 import './styles/code.css'
 import './styles/documentation.css'
@@ -21,10 +24,17 @@ import './styles/tailwind.css'
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const versions = (await getVersions()) ?? []
 
+  const algoliaAppId = process.env.ALGOLIA_APP_ID
+  const algoliaApiKey = process.env.ALGOLIA_API_KEY
+
   return json({
     requestInfo: {
       hints: getHints(request),
       userPrefs: { theme: getTheme(request) },
+    },
+    env: {
+      algoliaAppId,
+      algoliaApiKey,
     },
     versions,
   })
@@ -32,7 +42,17 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 // If you want to wrap your app further or include some shared UI
 function App() {
-  return <Outlet />
+  return (
+    <KBarProvider
+      options={{
+        disableDocumentLock: false,
+        disableScrollbarManagement: true,
+      }}
+    >
+      <Search />
+      <Outlet />
+    </KBarProvider>
+  )
 }
 
 export default function Document() {
@@ -45,11 +65,38 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <ClientHints />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@docsearch/css@3"
+        />
         <Links />
       </head>
       <body className="bg-background-color">
         <App />
+        <Progress />
         <ScrollRestoration />
+        <script src="https://cdn.jsdelivr.net/npm/@docsearch/js@3" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            docsearch({
+              apiKey: "${process.env.ALGOLIA_API_KEY}",
+              indexName: "remix-run",
+              inputSelector: "#search",
+              debug: false,
+              algoliaOptions: {
+                hitsPerPage: 5,
+              },
+              transformItems: (items) => {
+                return items.map((item) => {
+                  item.url = item.url.replace('https://remix.run', '');
+                  return item;
+                });
+              },
+            });
+          `,
+          }}
+        />
         <Scripts />
       </body>
     </html>
