c36ef3399b7080df55a51460add569ef13b0ed46
ShafSpecs <abbadshaf05@gmail.com>
2024-12-19 01:10:37 +0100

    📖 fixed links across the docs to match new convention

--- tests/e2e/link-checker.test.ts
+++ tests/e2e/link-checker.test.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test'
 test('doc navigation test', async ({ page }) => {
   await page.goto('/docs/main')
   await page.click("text=Beginner's Guide")
-  await expect(page).toHaveURL('/docs/main/quick-start')
+  await expect(page).toHaveURL('/docs/latest/quick-start')
 })
